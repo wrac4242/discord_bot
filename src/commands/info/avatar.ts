50f632e1052a997d80196bf12edf4451659cf4c5
wrac4242 <wrac4242@gmail.com>
2021-05-02 10:55:45 +0100

    add avatar command

---
+++ src/commands/info/avatar.ts
@@ -0,0 +1,14 @@
+import Discord = require('discord.js');
+
+module.exports = {
+	name: 'avatar',
+	description: 'gives the avatar of the running user',
+    guildOnly: false,
+    usage: '',
+    args: false,
+	permissions: 0,
+    aliases: ['pfp'],
+	execute(message: Discord.Message, _: string[]) {
+		return message.channel.send(message.author.displayAvatarURL({dynamic: true}));
+	},
+};
