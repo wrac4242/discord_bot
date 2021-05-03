5ba68776b8ff75f24a7d153dd66f89cb946e7322
wrac4242 <wrac4242@gmail.com>
2021-05-03 16:10:34 +0100

    add source command, add sourceRepo to config

---
+++ src/commands/info/source.ts
@@ -0,0 +1,14 @@
+import Discord = require('discord.js');
+const { sourceRepo } = require('../../../config.json');
+
+module.exports = {
+	name: 'source',
+	description: 'gives a link to the source code',
+    usage: '',
+    args: false,
+	permissions: 0,//5 // bot owner 4 // bot admin 3 // server owner 2 // server admin 1 server mod 0 // server member
+    aliases: ['sauce'],
+	execute(message: Discord.Message, args: string[]) {
+		return message.channel.send(`Source: <${sourceRepo}>`);
+	},
+};
