54b26d5f5c98ac4543674bc794dc1393683232e5
wrac4242 <wrac4242@gmail.com>
2021-05-03 11:14:02 +0100

    add an owner check to kill

--- src/commands/utility/kill.ts
+++ src/commands/utility/kill.ts
@@ -1,4 +1,5 @@
 import Discord = require('discord.js');
+const { admins } = require('../../../config.json');
 
 module.exports = {
 	name: 'kill',
@@ -9,6 +10,7 @@ module.exports = {
 	permissions: 5, // bot owner 4 // bot admin 3 // server owner 2 // server admin 1 server mod 0 // server member
     aliases: [],
 	execute(message: Discord.Message, _: string[]) {
+		if (message.author.id != admins[0]) return;
         console.log("killing")
 		message.channel.send(`Stopping the bot in a few seconds`);
         console.error(`Bot shutdown requested by ${message.author.tag}`);
